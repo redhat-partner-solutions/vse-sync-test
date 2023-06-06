7bbb14d37c251f0e876031f9815c704cc50226c9
Jose Nuñez <jnunyez@users.noreply.github.com>
2023-06-06 16:59:46 +0200

    ref implementation input test spec

--- tests/sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC/testimpl.py
+++ tests/sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC/testimpl.py
@@ -9,25 +9,22 @@ sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC
 
 from argparse import ArgumentParser
 import json
-from os.path import join as joinpath
-from os.path import dirname
 
 from vse_sync_pp.common import JsonEncoder
 from vse_sync_pp.parsers.ts2phc import TimeErrorParser
 from vse_sync_pp.analyzers.ts2phc import TimeErrorAnalyzer
 from vse_sync_pp.analyzers.analyzer import Config
 
-CONFIG = joinpath(dirname(__file__), 'config.yaml')
-
-def refimpl(filename, encoding='utf-8'):
+def refimpl(spec, filename, encoding='utf-8'):
     """A reference implementation of test:
 
     sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC
 
-    Return a dict with test result, reason, analysis of logs in `filename`.
+    Return a dict with test result, reason, analysis of logs in `filename`
+    with requirements specified in `spec`.
     """
     parser = TimeErrorParser()
-    analyzer = TimeErrorAnalyzer(Config.from_yaml(CONFIG))
+    analyzer = TimeErrorAnalyzer(Config.from_yaml(spec))
     with open(filename, encoding=encoding) as fid:
         analyzer.collect(*parser.parse(fid))
     return {
@@ -39,9 +36,10 @@ def refimpl(filename, encoding='utf-8'):
 def main():
     """Run this test and print test output as JSON to stdout"""
     aparser = ArgumentParser(description=main.__doc__)
+    aparser.add_argument('spec',help="test specification file")
     aparser.add_argument('input', help="log file to analyze")
     args = aparser.parse_args()
-    output = refimpl(args.input)
+    output = refimpl(args.spec, args.input)
     print(json.dumps(output, cls=JsonEncoder))
 
 if __name__ == '__main__':
