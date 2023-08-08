9fded8c2b3013a4a8ed709293d4e841b26cebf74
Charlie Wheeler-Robinson <cwheeler@redhat.com>
2023-08-08 13:29:00 -0400

    add all tests and output formats to e2e
    Signed-off-by: Charlie Wheeler-Robinson <cwheeler@redhat.com>

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -2,7 +2,7 @@
 # Start in the directory containing the `vse-sync-test` and `vse-sync-collection-tools` repositories.
 set -e
 # defaults
-DURATION=1500s
+DURATION=2000s
 
 usage() {
 	read -d '' usage_prompt <<- EOF
@@ -55,23 +55,33 @@ check_vars() {
     TESTROOT=$(pwd)
     COLLECTORPATH=$TESTROOT/vse-sync-collection-tools
     ANALYSERPATH=$TESTROOT/vse-sync-test
-    DATADIR=$TESTROOT/data # TODO add timestamp suffix
-    RESULTSDIR=$TESTROOT/junit
     TDPATH=$TESTROOT/testdrive/src
     PPPATH=$ANALYSERPATH/vse-sync-pp/src
 
+    DATADIR=$TESTROOT/data
+    RESULTSDIR=$DATADIR/results
+
     mkdir -p $DATADIR 
     mkdir -p $RESULTSDIR
+
+    ENVJSON="${RESULTSDIR}/env.json"
+    TESTJSON="${RESULTSDIR}/test.json"
+
+    ENVADOC="${RESULTSDIR}/env.adoc"
+    TESTADOC="${RESULTSDIR}/test.adoc"
+
+    ENVJUNIT="${RESULTSDIR}/env.junit"
+    TESTJUNIT="${RESULTSDIR}/test.junit"
+    FULLJUNIT="${RESULTSDIR}/combined.junit"
 }
 
 verify_env(){
   echo "Verifying test env. Please wait..."
   cd $COLLECTORPATH
   dt=$(date --rfc-3339='seconds' -u)
-  junit_template=$(echo ".[] | {\"id\": .data[0], \"result\": .data[1], \"reason\": .data[2], \"analysis\": .data[3], \"timestamp\": \"${dt}\", "time": 0}")
-  go run main.go env verify --interface="${INTERFACE_NAME}" --kubeconfig="${LOCAL_KUBECONFIG}" --use-analyser-format | \
-    jq -s -c "${junit_template}" | \
-    PYTHONPATH=$TDPATH python3 -m testdrive.junit --prettify "env" - >  $RESULTSDIR/env.junit
+  junit_template=$(echo ".[].data + { \"timestamp\": \"${dt}\", "time": 0}")
+  go run main.go env verify --interface="${INTERFACE_NAME}" --kubeconfig="${LOCAL_KUBECONFIG}" --use-analyser-format >> ${ENVJSON}.raw
+  cat ${ENVJSON}.raw | jq -s -c "${junit_template}" >> ${ENVJSON}
 }
 
 collect_data() {
@@ -81,32 +91,44 @@ collect_data() {
     go run main.go logs -k="${LOCAL_KUBECONFIG}" -o="${DATADIR}" --since="${DURATION}"
 }
 
-analyse_data_junit() {
-    GNSS_DEMUXED_PATH=$DATADIR/gnss-terror.demuxed
+analyse_data() {
     PTP_DAEMON_LOGFILE=$(ls -tr1 $DATADIR/linuxptp-daemon-container-* | tail -n 1)
 
-    PYTHONPATH=$PPPATH python3 -m vse_sync_pp.demux  $DATADIR/collected.log 'gnss/time-error' > $GNSS_DEMUXED_PATH
+    GNSS_DEMUXED_PATH=$DATADIR/gnss-terror.demuxed
+    DPLL_DEMUXED_PATH=$DATADIR/dpll-terror.demuxed
 
+    PYTHONPATH=$PPPATH python3 -m vse_sync_pp.demux  $DATADIR/collected.log 'gnss/time-error' >> $GNSS_DEMUXED_PATH
+    PYTHONPATH=$PPPATH python3 -m vse_sync_pp.demux  $DATADIR/collected.log 'dpll/time-error' >> $DPLL_DEMUXED_PATH
+
+    cd ${ANALYSERPATH}
     DRIVE_TESTS_JSON="tests.json"
     SPACER="    "
-
     echo "[" > $DRIVE_TESTS_JSON
-    echo "${SPACER}[" >> $DRIVE_TESTS_JSON
-    echo "${SPACER} ${SPACER} \"${ANALYSERPATH}/tests/sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC/PRTC-A/testimpl.py\", \"${PTP_DAEMON_LOGFILE}\"" >> $DRIVE_TESTS_JSON
-    echo "${SPACER}]," >> $DRIVE_TESTS_JSON 
-    echo "${SPACER}[" >> $DRIVE_TESTS_JSON
-    echo "${SPACER} ${SPACER} \"${ANALYSERPATH}/tests/sync/G.8272/time-error-in-locked-mode/Constellation-to-GNSS-receiver/PRTC-A/testimpl.py\", \"${GNSS_DEMUXED_PATH}\"" >> $DRIVE_TESTS_JSON
-    echo "${SPACER}]" >> $DRIVE_TESTS_JSON
+    echo "${SPACER}[\"tests/sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC/PRTC-A/testimpl.py\", \"${PTP_DAEMON_LOGFILE}\"]," >> $DRIVE_TESTS_JSON
+    echo "${SPACER}[\"tests/sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC/PRTC-B/testimpl.py\", \"${PTP_DAEMON_LOGFILE}\"]," >> $DRIVE_TESTS_JSON
+    echo "${SPACER}[\"tests/sync/G.8272/time-error-in-locked-mode/PHC-to-SYS/RAN/testimpl.py\", \"${PTP_DAEMON_LOGFILE}\"]," >> $DRIVE_TESTS_JSON
+    echo "${SPACER}[\"tests/sync/G.8272/time-error-in-locked-mode/Constellation-to-GNSS-receiver/PRTC-A/testimpl.py\", \"${GNSS_DEMUXED_PATH}\"]," >> $DRIVE_TESTS_JSON
+    echo "${SPACER}[\"tests/sync/G.8272/time-error-in-locked-mode/Constellation-to-GNSS-receiver/PRTC-B/testimpl.py\", \"${GNSS_DEMUXED_PATH}\"]," >> $DRIVE_TESTS_JSON
+    echo "${SPACER}[\"tests/sync/G.8272/time-error-in-locked-mode/1PPS-to-DPLL/PRTC-A/testimpl.py\", \"${DPLL_DEMUXED_PATH}\"]," >> $DRIVE_TESTS_JSON
+    # Remember to fixup trailing commas if you amend this!
+    echo "${SPACER}[\"tests/sync/G.8272/time-error-in-locked-mode/1PPS-to-DPLL/PRTC-B/testimpl.py\", \"${DPLL_DEMUXED_PATH}\"]" >> $DRIVE_TESTS_JSON
     echo "]" >> $DRIVE_TESTS_JSON
 
-    env PYTHONPATH=$TDPATH:$PPPATH python3 -m testdrive.run https://github.com/redhat-partner-solutions/vse-sync-test/ $DRIVE_TESTS_JSON | \
-      env PYTHONPATH=$TDPATH python3 -m testdrive.junit --prettify "tests" - >  $RESULTSDIR/tests.junit
+    BRANCHNAME=$(git branch --show-current)
+
+    env PYTHONPATH=$TDPATH:$PPPATH python3 -m testdrive.run https://github.com/redhat-partner-solutions/vse-sync-test/tree/${BRANCHNAME} $DRIVE_TESTS_JSON >> ${TESTJSON}
 }
 
-merge_junit() {
-  cd $RESULTSDIR
-  junitparser merge * combined.junit
-  cat combined.junit
+create_junit() {
+  cat ${ENVJSON} | env PYTHONPATH=$TDPATH python3 -m testdrive.junit --prettify "Environment" - >  ${ENVJUNIT}
+  cat ${TESTJSON} | env PYTHONPATH=$TDPATH python3 -m testdrive.junit --prettify "T-GM Tests" - >  ${TESTJUNIT}
+
+  junitparser merge ${RESULTSDIR}/*.junit ${FULLJUNIT}
+}
+
+create_adoc() {
+  cat ${ENVJSON} | env PYTHONPATH=$TDPATH python3 -m testdrive.asciidoc "Environment" - >  ${ENVADOC}
+  cat ${TESTJSON} | env PYTHONPATH=$TDPATH python3 -m testdrive.asciidoc "T-GM Tests" - >  ${TESTADOC}
 }
 
 # Parge args beginning with -
@@ -142,5 +164,11 @@ done
 check_vars
 verify_env
 collect_data
-analyse_data_junit
-merge_junit
+analyse_data
+create_junit
+create_adoc
+
+cat ${ENVADOC}
+cat ${TESTADOC}
+
+cat ${FULLJUNIT}
