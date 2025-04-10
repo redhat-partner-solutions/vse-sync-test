76e7898453054a1a03fc741aa5f5921f6ed912c4
Michele Costa <mtcosta@protonmail.com>
2025-04-10 13:03:38 +0100

    Allow for different patten of ts2phc and allow test implementations to pass interface
    In newer ts2phc logs master is no longer added to the offset line
    Interface is required for multicard senarios

--- tests/sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC/testimpl.py
+++ tests/sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC/testimpl.py
@@ -26,14 +26,14 @@ from vse_sync_pp.analyzers.analyzer import Config
 CONFIG = joinpath(dirname(__file__), 'config.yaml')
 
 
-def refimpl(filename, encoding='utf-8'):
+def refimpl(filename, interface=None, encoding='utf-8'):
     """A reference implementation for tests under:
 
     sync/G.8272/time-error-in-locked-mode/DPLL-to-PHC
 
     Return a dict with test result, reason, timestamp, duration, and analysis of logs in `filename`.
     """
-    parser = TimeErrorParser()
+    parser = TimeErrorParser(interface)
     analyzer = TimeErrorAnalyzer(Config.from_yaml(CONFIG))
     with open_input(filename, encoding=encoding) as fid:
         analyzer.collect(*parser.parse(fid))
@@ -50,8 +50,9 @@ def main():
     """Run this test and print test output as JSON to stdout"""
     aparser = ArgumentParser(description=main.__doc__)
     aparser.add_argument('input', help="log file to analyze")
+    aparser.add_argument('interface', help="interface to capture", default=None)
     args = aparser.parse_args()
-    output = refimpl(args.input)
+    output = refimpl(args.input, args.interface)
     # Python exits with error code 1 on EPIPE
     if not print_loj(output):
         sys.exit(1)
