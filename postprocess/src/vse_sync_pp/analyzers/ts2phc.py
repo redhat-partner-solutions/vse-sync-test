d8b8c3e5f48c7f18c40e4e09762664b344187d49
Michele Costa <mtcosta@protonmail.com>
2025-04-14 09:48:55 +0100

    consider s3 state as locked in ts2phc analyzers

--- postprocess/src/vse_sync_pp/analyzers/ts2phc.py
+++ postprocess/src/vse_sync_pp/analyzers/ts2phc.py
@@ -10,7 +10,7 @@ class TimeErrorAnalyzer(TimeErrorAnalyzerBase):
     """Analyze time error"""
     id_ = 'ts2phc/time-error'
     parser = id_
-    locked = frozenset({'s2'})
+    locked = frozenset({'s2', 's3'})
 
     def test(self, data):
         return self._check_missing_samples(data, *super().test(data))
@@ -20,7 +20,7 @@ class TimeDeviationAnalyzer(TimeDeviationAnalyzerBase):
     """Analyze time deviation"""
     id_ = 'ts2phc/time-deviation'
     parser = 'ts2phc/time-error'
-    locked = frozenset({'s2'})
+    locked = frozenset({'s2', 's3'})
 
     def test(self, data):
         return self._check_missing_samples(data, *super().test(data))
@@ -30,7 +30,7 @@ class MaxTimeIntervalErrorAnalyzer(MaxTimeIntervalErrorAnalyzerBase):
     """Analyze max time interval error"""
     id_ = 'ts2phc/mtie'
     parser = 'ts2phc/time-error'
-    locked = frozenset({'s2'})
+    locked = frozenset({'s2', 's3'})
 
     def test(self, data):
         return self._check_missing_samples(data, *super().test(data))
