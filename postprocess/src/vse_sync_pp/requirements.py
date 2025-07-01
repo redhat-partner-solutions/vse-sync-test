34f205d30630da9b85ff05583182767110682d2e
vkhokhar <vkhokhar@redhat.com>
2025-07-02 00:19:24 +0530

    Add G.8273.2 requirements and adds basic tests for T-BC and T-TSC

--- postprocess/src/vse_sync_pp/requirements.py
+++ postprocess/src/vse_sync_pp/requirements.py
@@ -36,4 +36,13 @@ REQUIREMENTS = {
             (None, 100000): lambda t: 1
         }
     },
+    'G.8273.2/Class-C': {
+        'maximum-time-interval-error-in-locked-mode/ns': {
+            (None, 1000): lambda t: 10,
+        },
+        'time-deviation-in-locked-mode/ns': {
+            (None, 1000): lambda t: 2,
+        },
+        'time-error-in-locked-mode/ns': 30,
+    },
 }
