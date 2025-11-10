97e4daeb91a76a6eb2bcb132c65317bc089f0d9a
vkhokhar <vkhokhar@redhat.com>
2025-11-10 19:41:42 +0530

    test

--- postprocess/tests/vse_sync_pp/parsers/test_ptp4l.py
+++ postprocess/tests/vse_sync_pp/parsers/test_ptp4l.py
@@ -73,46 +73,8 @@ class TestTimeErrorParserWithInterface(TestCase, metaclass=ParserTestBuilder):
             "ptp4l[681011.839]: [ptp4l.0.config] enp2s0f0 offset -23947 s0 freq +0 path delay 11350",
             (Decimal("681011.839"), "enp2s0f0", -23947, "s0", 0, 11350),
         ),
+        ( 
+            ""
     )
     reject = ()
     discard = ()
-
-
-class TestPortStateParser(TestCase, metaclass=ParserTestBuilder):
-    """Test cases for vse_sync_pp.parsers.ptp4l.PortStateParser"""
-
-    constructor = PortStateParser
-    id_ = "ptp4l/port-state"
-    elems = ("timestamp", "port", "from_state", "to_state", "event")
-    accept = (
-        (
-            "ptp4l[681011.839]: [ptp4l.0.config] "
-            "port 1: INITIALIZING to LISTENING on INIT_COMPLETE",
-            (Decimal("681011.839"), 1, "INITIALIZING", "LISTENING", "INIT_COMPLETE"),
-        ),
-        (
-            "ptp4l[681012.840]: "
-            "port 1: LISTENING to UNCALIBRATED on RS_SLAVE",
-            (Decimal("681012.840"), 1, "LISTENING", "UNCALIBRATED", "RS_SLAVE"),
-        ),
-        (
-            "ptp4l[681013.841]: "
-            "port 1: UNCALIBRATED to SLAVE on MASTER_CLOCK_SELECTED",
-            (Decimal("681013.841"), 1, "UNCALIBRATED", "SLAVE", "MASTER_CLOCK_SELECTED"),
-        ),
-        (
-            "ptp4l[681014.842]: "
-            "port 2: LISTENING to MASTER on ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES",
-            (Decimal("681014.842"), 2, "LISTENING", "MASTER", "ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES"),
-        ),
-    )
-    reject = (
-        # Missing port number
-        "ptp4l[681011.839]: INITIALIZING to LISTENING on INIT_COMPLETE",
-        # Wrong format
-        "ts2phc[681011.839]: port 1: INITIALIZING to LISTENING on INIT_COMPLETE",
-        # Missing event
-        "ptp4l[681011.839]: port 1: INITIALIZING to LISTENING",
-        # Invalid timestamp
-        "ptp4l[invalid]: port 1: INITIALIZING to LISTENING on INIT_COMPLETE",
-    ) 
