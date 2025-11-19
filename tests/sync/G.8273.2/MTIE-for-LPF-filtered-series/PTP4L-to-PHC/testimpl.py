9e1cae503eda1da48fee34201611fc4a7222743b
vkhokhar <vkhokhar@redhat.com>
2025-11-19 12:14:59 +0530

    newconfigadded

--- tests/sync/G.8273.2/MTIE-for-LPF-filtered-series/PTP4L-to-PHC/testimpl.py
+++ tests/sync/G.8273.2/MTIE-for-LPF-filtered-series/PTP4L-to-PHC/testimpl.py
@@ -50,7 +50,6 @@ def main():
     """Run this test and print test output as JSON to stdout"""
     aparser = ArgumentParser(description=main.__doc__)
     aparser.add_argument('input', help="log file to analyze")
-    #aparser.add_argument('interface', help="interface to capture", default=None)
     args = aparser.parse_args()
 
     output = refimpl(args.input, config=CONFIG)
