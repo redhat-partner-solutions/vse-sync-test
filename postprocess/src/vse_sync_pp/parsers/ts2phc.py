124da51a2abf04613a2ac5ef1aa715b0ed2954bb
Michele Costa <mtcosta@protonmail.com>
2026-03-30 20:36:59 +0100

    Fix ts2phc sampling by removing dpll logs

--- postprocess/src/vse_sync_pp/parsers/ts2phc.py
+++ postprocess/src/vse_sync_pp/parsers/ts2phc.py
@@ -9,12 +9,7 @@ from .parser import (Parser, parse_decimal)
 
 
 class TimeErrorParser(Parser):
-    """Parse time error from ts2phc or dpll log messages.
-
-    Supports both formats:
-    - Old ts2phc format: ts2phc[681011.839]: [ts2phc.0.config] ens3f0 master offset 0 s2
-    - New dpll format (PTP operator 4.16+ with e810 plugin):
-      dpll[1769630949]:[ts2phc.0.config] ens3f0 frequency_status 3 offset 0 phase_status 3 pps_status 1 s2
+    """Parse time error from ts2phc log messages.
 
     The interface parameter can be a single string, or an iterable of strings
     to match any of several identifiers (e.g. both a PTP clock path like
@@ -44,8 +39,8 @@ class TimeErrorParser(Parser):
         return fr'\s({"|".join(identifiers)})'
 
     @staticmethod
-    def build_regexp_ts2phc(interface=None):
-        """Return a regular expression for parsing old ts2phc log format."""
+    def build_regexp(interface=None):
+        """Return a regular expression for parsing ts2phc log format."""
         return r''.join((
             r'^ts2phc'
             + r'\[([1-9][0-9]*\.?[0-9]*)\]:', # timestamp (with or without decimal)
@@ -57,26 +52,9 @@ class TimeErrorParser(Parser):
             r'.*$',
         ))
 
-    @staticmethod
-    def build_regexp_dpll(interface=None):
-        """Return a regular expression for parsing dpll log format (e810 plugin)."""
-        return r''.join((
-            r'^dpll'
-            + r'\[([1-9][0-9]*)\]:', # timestamp (integer, unix epoch)
-            r'\[.*\]',  # configuration file name like [ts2phc.0.config]
-            TimeErrorParser._interface_pattern(interface),
-            r'\s+frequency_status\s+\d+',  # frequency_status field
-            r'\s+offset\s+(-?[0-9]+)',  # time error (offset value)
-            r'\s+phase_status\s+\d+',  # phase_status field
-            r'(?:\s+pps_status\s+\d+)?',  # optional pps_status field
-            r'\s+(\S+)', # state
-            r'.*$',
-        ))
-
     def __init__(self, interface=None):
         super().__init__()
-        self._regexp_ts2phc = re.compile(self.build_regexp_ts2phc(interface))
-        self._regexp_dpll = re.compile(self.build_regexp_dpll(interface))
+        self._regexp = re.compile(self.build_regexp(interface))
 
     def make_parsed(self, elems):
         if len(elems) < len(self.elems):
@@ -88,17 +66,7 @@ class TimeErrorParser(Parser):
         return self.parsed(timestamp, interface, terror, state)
 
     def parse_line(self, line):
-        # Try old ts2phc format first
-        matched = self._regexp_ts2phc.match(line)
-        if matched:
-            return self.make_parsed((
-                matched.group(1),
-                matched.group(2),
-                matched.group(3),
-                matched.group(4),
-            ))
-        # Try new dpll format (PTP operator 4.16+ with e810 plugin)
-        matched = self._regexp_dpll.match(line)
+        matched = self._regexp.match(line)
         if matched:
             return self.make_parsed((
                 matched.group(1),
